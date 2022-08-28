26820f77bfbc33ebc191f5451a9ceeafd15e34d8
WickedChar0nVI <90307314+WickedCharonVI@users.noreply.github.com>
2022-08-28 18:27:07 +0530

    Update Ticketing Counter.py

--- Ticketing Counter.py
+++ Ticketing Counter.py
@@ -1,12 +1,24 @@
-height = int(input("Enter your Height in Cm's:\n"))
-if height >= 120:
-    print("Yes You Can Ride!!!")
-    age = int(input("Please Enter your Age:\n"))
-    if age >= 18: 
-        print("Please Pay 1000/-")
-    elif age >= 12 and age < 18:
-        print("Please Pay 600/-")
-    else:
-        print("Please Pay 400/-")
+print("Welcome to Rollercoaster!!")
+height = int(input("May I know your Height in cm's:\n"))
+bill = 0
+if height <= 120:
+    print("Sorry, You Can't ride!!!")
 else:
-    print("Sorry you Cant Ride")
+    print("Yes,You Can Ride!!!")
+    age = int(input("May I know your Age:\n"))
+    if age >= 18:
+        bill = 1200
+        print("You Need to Pay 1200/-")
+    elif age > 12 and age < 18:
+        bill = 700
+        print("You Need to pay 700/-")
+    else:
+        bill = 500
+        print("You need to pay 500/-")
+
+    pics = input("Do you want any pic's? yes or no:\n")
+    if pics == "yes":
+        bill +=  300
+        print(f"Your Bill is {bill}")
+    else:
+        print(f"Your bill is {bill}")
